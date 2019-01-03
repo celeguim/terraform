1b2dfc33ae7f22474b15ed0b7e43a6b20a273311
celeguim <luiz.celeguim@gmail.com>
2019-01-03 15:35:44 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,19 @@ output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
 
+output "private subnet" {
+  value = "${formatlist("%v", aws_subnet.private_subnet.*.id)}"
+}
+
+output "public subnet" {
+  value = "${formatlist("%v", aws_subnet.public_subnet.*.id)}"
+}
+
+output "private route" {
+  value = "${formatlist("%v", aws_route_table.private.*.id)}"
+}
+
+output "public route" {
+  value = "${formatlist("%v", aws_route_table.public.*.id)}"
+}
+
--- vpc.tf
+++ vpc.tf
@@ -26,7 +26,25 @@ resource "aws_subnet" "private_subnet" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "${var.private_subnet_list[count.index]}"
   availability_zone = "${element(var.az_list, count.index)}"
-  tags = "${merge(map("Name", format("%s-${var.private_subnet_suffix}-%s", var.vpc_name, element(var.az_list, count.index))), var.tags, var.private_subnet_tags)}"
+  tags = "${merge(map("Name", format("%s-${var.private_subnet_suffix}", element(var.az_list, count.index))))}"
+}
+
+#################
+# Private routes
+#################
+resource "aws_route_table" "private" {
+  count = "${local.max_subnet_length}"
+  vpc_id = "${local.vpc_id}"
+  tags = "${merge(map("Name", format("%s-${var.private_subnet_suffix}", element(var.az_list, count.index))))}"
+}
+
+###################################
+# Private Route table association
+###################################
+resource "aws_route_table_association" "private" {
+  count = "${length(var.private_subnet_list)}"
+  subnet_id = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, (var.single_nat_gateway ? 0 : count.index))}"
 }
 
 ################
@@ -37,14 +55,46 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "${var.public_subnet_list[count.index]}"
   availability_zone = "${element(var.az_list, count.index)}"
-  tags = "${merge(map("Name", format("%s-${var.public_subnet_suffix}-%s", var.vpc_name, element(var.az_list, count.index))), var.tags, var.public_subnet_tags)}"
+  tags = "${merge(map("Name", format("%s-${var.public_subnet_suffix}", element(var.az_list, count.index))))}"
 }
 
 #################
-# Private routes
+# Public routes
 #################
-resource "aws_route_table" "private" {
+resource "aws_route_table" "public" {
+#  count = "${length(var.public_subnet_list) > 0 ? 1 : 0}"
   count = "${local.max_subnet_length}"
   vpc_id = "${local.vpc_id}"
-  tags = "${merge(map("Name", (var.single_nat_gateway ? "${var.vpc_name}-${var.private_subnet_suffix}" : format("%s-${var.private_subnet_suffix}-%s", var.vpc_name, element(var.az_list, count.index)))), var.tags, var.private_route_table_tags)}"
+  tags = "${merge(map("Name", format("%s-${var.public_subnet_suffix}", element(var.az_list, count.index))))}"
 }
+
+resource "aws_route" "public_internet_gateway" {
+  destination_cidr_block = "0.0.0.0/0"
+  count = "${local.max_subnet_length}"
+  route_table_id = "${element(aws_route_table.public.*.id, count.index)}"
+  gateway_id = "${aws_internet_gateway.public.id}"
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+###################################
+# Public Route table association
+###################################
+resource "aws_route_table_association" "public" {
+  count = "${length(var.public_subnet_list)}"
+  subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.public.*.id, (var.single_nat_gateway ? 0 : count.index))}"
+}
+
+###################
+# Internet Gateway
+###################
+resource "aws_internet_gateway" "public" {
+  vpc_id = "${local.vpc_id}"
+  tags {
+    Name = "${var.aws_region}-${var.public_subnet_suffix}"
+  }
+}
+
